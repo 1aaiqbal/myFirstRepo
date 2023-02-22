5a3720c325ea2f5e8d3965804316b51acc7ee389
1aaiqbal <125518514+1aaiqbal@users.noreply.github.com>
2023-02-21 20:02:53 -0500

    Update GitPractieceTwo.java
    This for for pull practice

--- src/test/GitPractieceTwo.java
+++ src/test/GitPractieceTwo.java
@@ -4,6 +4,7 @@ public class GitPractieceTwo {
 	public static void main(String[] args) {
 		
 System.out.println("This is for push practice");
+System.out.println("This is for pull practice");
 		
 		
 	}
